2064d894ad8f71686aef53ba74d04cc7b358bbfb
= <edemwomagno@gmail.com>
2024-11-12 12:06:26 +0000

    simplon

--- app/Http/Controllers/OpinionController.php
+++ app/Http/Controllers/OpinionController.php
@@ -23,13 +23,12 @@ class OpinionController extends Controller
             $opinion->opinion = $request->opinion;
             $opinion->rate = $request->rate;
             $opinion->save();
-    
+
             // Retourner la réponse JSON
             return response()->json([
                 'opinion' => $opinion,
                 'message' => 'Opinion publiée avec succès !'
             ], 201);
-   
         } catch (\Illuminate\Validation\ValidationException $e) {
             // Retourner les erreurs de validation
             return response()->json([
@@ -39,39 +38,33 @@ class OpinionController extends Controller
     }
 
     public function getOpinions()
-{
-    try {
-        // Récupérer les opinions avec les informations de l'utilisateur associé
-        $opinions = Opinion::with('user')->get(); // Utilisation de `get()` pour récupérer toutes les opinions sans pagination
+    {
+        try {
 
-        // Ajouter dd() pour déboguer et inspecter la réponse
-        // dd($opinions); // Cela arrêtera l'exécution et affichera les données
+            $opinions = Opinion::with('user')->latest()->take(3)->get();
 
-        $opinionsData = $opinions->map(function ($opinion) {
-            return [
-                'id' => $opinion->id,
-                'opinion' => $opinion->opinion,
-                'rate' => $opinion->rate,
-               'user_name' => $opinion->user->firstname . ' ' . $opinion->user->lastname,  // Utilisation de la relation 'user' pour récupérer le nom
-               'avatar' => $opinion->user->avatar,
-               
+            // Création d'un tableau avec des données formatées pour la réponse JSON
+            //si le tableau est vide
 
-            
-            ];
-        });
+            $opinionsData = $opinions->map(function ($opinion) {
+                return [
+                    'id' => $opinion->id,
+                    'opinion' => $opinion->opinion,
+                    'rate' => $opinion->rate,
+                    'user_name' => $opinion->user->firstname . ' ' . $opinion->user->lastname,  // Utilisation de la relation 'user' pour récupérer le nom
+                    'avatar' => $opinion->user->avatar,
+                ];
+            });
 
-        return response()->json([
-
-            'data' => $opinionsData
-        ], 200);
-
-    } catch (\Exception $e) {
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Erreur lors de la récupération des opinions.',
-            'error' => $e->getMessage()
-        ], 500);
+            return response()->json([
+                'data' => $opinionsData
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Erreur lors de la récupération des opinions.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
-}
-
 }
